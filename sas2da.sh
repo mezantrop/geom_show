7d1ef375e6391a87a86bc5d7bd78b1a9b71d0b1c
Mikhail Zakharov <zmey20000@yahoo.com>
2018-06-01 18:19:06 +0200

    Create sas2da.sh

---
+++ sas2da.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# -----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE" (Revision 42):
+# zmey20000@yahoo.com wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return Mikhail Zakharov
+# -----------------------------------------------------------------------------
+
+# SAS addresses to /dev/daXXs with drive sizes + drive serials and vendor info
+#
+# sas2da.sh depends on geom_show.sh: https://github.com/mezantrop/geom_show
+#
+# 2018/06/01    v0.1    Initial
+#
+
+GEOM_SHOW="./geom_show.sh"
+
+gs=`mktemp /tmp/get_drives.XXX`
+$GEOM_SHOW -l -c DISK | awk -F, '{print $2, $3, $10, $11}' | sort > $gs
+# Provider Mediasize Ident Description
+# da0 8001563222016 VJGSNV0X HGST HUH728080AL5204
+# da1 8001563222016 VJGSNV0X HGST HUH728080AL5204
+# da10 8001563222016 VJGTAP3X HGST HUH728080AL5204
+# ...
+
+# Filter SCSI BUS and Target IDs on SES devices (drive enclosures) only
+scbus_targets=`camcontrol devlist | 
+        awk 'BEGIN {FS="scbus|target |lun"} /ses/ {print $2, $3}'`
+# 4  0 
+# 11  0 
+# 12  28 
+# ...
+
+da_addr=`mktemp /tmp/get_drives.XXX`
+printf "$scbus_targets\n" | while read bus target; do
+        camcontrol smpphylist "$bus:$target" -l -q 2>/dev/null |
+                grep -v 'ses' | 
+                awk 'BEGIN{FS="0x|  <.*>|[(,)]"} {print $4, $2}' 
+done | sort > $da_addr
+# da0 5000cca2612b0b0d
+# da1 5000cca2612b0b0e
+# da10 5000cca2612c4402
+# ...
+
+echo "Address Dev Size Serial Vendor Model"
+
+join $da_addr $gs | awk '{print $2, $1, $3, $4, $5, $6}' | sort -k 4 
+# 5000cca2612b0b0d da0 8001563222016 VJGSNV0X HGSTHUH728080AL5204
+# 5000cca2612b0b0e da1 8001563222016 VJGSNV0X HGSTHUH728080AL5204
+# 5000cca2612c4402 da10 8001563222016 VJGTAP3X HGSTHUH728080AL5204
+# 5000cca2612c4401 da11 8001563222016 VJGTAP3X HGSTHUH728080AL5204
+# 5000cca26128680e da12 8001563222016 VJGR6WGX HGSTHUH728080AL5204
+# ...
+
+rm $gs $da_addr 
